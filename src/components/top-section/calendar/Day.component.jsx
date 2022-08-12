9e91c83610e996cbe9a4765b403bedacd711df5e
Hongxu Zha <tomzha0403@gmail.com>
2022-08-12 11:21:09 +0800

    update: generateData() in utils/time.js for clicking grayed out dates

--- src/components/top-section/calendar/Day.component.jsx
+++ src/components/top-section/calendar/Day.component.jsx
@@ -2,43 +2,66 @@ import React, { useState, useContext } from "react";
 import { CalendarContext } from "../../../context/calendar.context";
 
 function Day(props) {
-    const { setGDay, gDay } = useContext(CalendarContext);
-    // const [hasEvent, setHasEvent] = useState(false);
-    let txtColor = "black";
-    const fontWeight = props.hasEvent ? "bold" : "normal";
-    const bgColor = props.isToday ? "lightblue" : "white";
-    const bdColor = props.isSelected ? "red" : "black";
-    const [isHover, setIsHover] = useState(bdColor);
+  const { setGDay, gDay } = useContext(CalendarContext);
+  // const [hasEvent, setHasEvent] = useState(false);
+  let txtColor = "black";
+  const fontWeight = props.hasEvent ? "bold" : "normal";
+  const bgColor = props.isToday ? "lightblue" : "white";
+  const bdColor = props.isSelected ? "red" : "black";
+  const [isHover, setIsHover] = useState(bdColor);
 
-    if (props.isInThisMonth !== "this") {
-        txtColor = "darkgray";
-    }
-    let nameISO = props.name;
-    if (Number(props.name) < 10) {
-        nameISO = "0" + props.name;
-    }
-    const divStyle = {
-        fontSize: "large",
-        color: txtColor,
-        fontWeight: fontWeight,
-        backgroundColor: bgColor,
-        borderColor: isHover,
-        borderRadius: "20% / 50%",
-    };
+  if (props.isInThisMonth !== "this") {
+    txtColor = "darkgray";
+  }
+  let nameISO = props.name;
+  if (Number(props.name) < 10) {
+    nameISO = "0" + props.name;
+  }
+  const divStyle = {
+    fontSize: "large",
+    color: txtColor,
+    fontWeight: fontWeight,
+    backgroundColor: bgColor,
+    borderColor: isHover,
+    borderRadius: "20% / 50%",
+  };
 
-    function handleClick() {
-        if (props.isInThisMonth === "this") {
-            setGDay(gDay.slice(0, 8)+nameISO);
+  function handleClick() {
+    if (props.isInThisMonth === "this") {
+      setGDay(gDay.slice(0, 8) + nameISO);
+    } else {
+      let monthNum;
+      let yearNum = Number(gDay.slice(0, 4));
+      if (props.isInThisMonth === "last") {
+        monthNum = Number(gDay.slice(5, 7)) - 1;
+        if (monthNum === 0) {
+          yearNum -= 1; // back to last year.
+          monthNum = 12;
         }
+      } else {
+        monthNum = Number(gDay.slice(5, 7)) + 1;
+        if (monthNum === 13) {
+          yearNum += 1;
+          monthNum = 1;
+        }
+      }
+      const newMonth =
+        monthNum < 10 ? "0" + String(monthNum) : String(monthNum);
+      setGDay(String(yearNum) + "-" + newMonth + "-" + nameISO);
     }
-    const chosen = (
-        <button id={props.id} className="dayBtn" style={divStyle}
-            onMouseEnter={() => setIsHover("lightcoral")} onMouseLeave={() => setIsHover(bdColor)} onClick={handleClick}>
-            {props.name}
-        </button>
-    );
-    return (
-        chosen
-    );
+  }
+  const chosen = (
+    <button
+      id={props.id}
+      className="dayBtn"
+      style={divStyle}
+      onMouseEnter={() => setIsHover("lightcoral")}
+      onMouseLeave={() => setIsHover(bdColor)}
+      onClick={handleClick}
+    >
+      {props.name}
+    </button>
+  );
+  return chosen;
 }
 export default Day;
--- src/utils/time.js
+++ src/utils/time.js
@@ -8,94 +8,97 @@ export const today = todayDate.slice(8, 10);
 export const thisMonth = todayDate.slice(5, 7);
 
 export function generateData(gDay, events, currentUser) {
-    const isThisMonth = thisMonth === gDay.slice(5, 7);
-    // {givenDate} is a string of ISO8062, the selected day.
-    const givenDate = new Date(gDay);
-    // The day of the start of the month of the selected day.
-    const startOfMonth = new Date(
-      givenDate.getFullYear(),
-      givenDate.getMonth(),
-      1
-    ).getDay();
-    // Mon=1, SUN=7.
-    const startDayOfMonth = startOfMonth === 0 ? 7 : startOfMonth;
-    // number of days in that month.
-    const daysInMonth = new Date(
-      givenDate.getFullYear(),
-      givenDate.getMonth() + 1,
-      0
-    ).getDate();
-    // number of days in last month.
-    const daysInLastMonth = new Date(
-      givenDate.getFullYear(),
-      givenDate.getMonth(),
-      0
-    ).getDate();
-    //
-    const eventsInThisMonth =
-      events &&
-      events.filter((event) => event.date.slice(5, 7) === gDay.slice(5, 7));
+  const isThisMonth = thisMonth === gDay.slice(5, 7);
+  // {givenDate} is a string of ISO8062, the selected day.
+  const givenDate = new Date(gDay);
+  // The day of the start of the month of the selected day.
+  const startOfMonth = new Date(
+    givenDate.getFullYear(),
+    givenDate.getMonth(),
+    1
+  ).getDay();
+  // Mon=1, SUN=7.
+  const startDayOfMonth = startOfMonth === 0 ? 7 : startOfMonth;
+  // number of days in that month.
+  const daysInMonth = new Date(
+    givenDate.getFullYear(),
+    givenDate.getMonth() + 1,
+    0
+  ).getDate();
+  // number of days in last month.
+  const daysInLastMonth = new Date(
+    givenDate.getFullYear(),
+    givenDate.getMonth(),
+    0
+  ).getDate();
+  //
+  const eventsInThisMonth =
+    events &&
+    events.filter((event) => event.date.slice(5, 7) === gDay.slice(5, 7));
 
-    function buildData(id, name, isToday, hasEvent, isSelected, isInThisMonth) {
-      const day = {
-        id: id,
-        name: name,
-        isToday: isToday,
-        hasEvent: hasEvent,
-        isSelected: isSelected,
-        isInThisMonth: isInThisMonth,
-      };
-      return day;
-    }
-
-    const ButtonList = [];
-
-    for (
-      let i = 1, j = daysInLastMonth - startDayOfMonth + 2;
-      i < startDayOfMonth;
-      i++, j++
-    ) {
-      const day = buildData(`${i}` + nanoid(), j, false, false, false, "last");
-      ButtonList.push(day);
-    }
-
-    let isTd,
-      isSl = false;
-    for (let i = 0; i < daysInMonth; i++) {
-      const eventI = currentUser
-        ? eventsInThisMonth.filter(
-            (event) => Number(event.date.slice(8, 10)) === i + 1
-          )
-        : [];
-
-      if (i + 1 === Number(today) && isThisMonth) {
-        isTd = true;
-      }
-      if (i + 1 === Number(gDay.slice(8, 10))) {
-        isSl = true;
-      }
-      const day = buildData(
-        String(i + startDayOfMonth) + nanoid(),
-        i + 1,
-        isTd,
-        eventI.length > 0,
-        isSl,
-        "this"
-      );
-      isTd = isSl = false;
-      ButtonList.push(day);
-    }
-
-    for (let i = ButtonList.length, j = 1; i < 35; i++, j++) {
-      const day = buildData(
-        String(i) + nanoid(),
-        j,
-        false,
-        false,
-        false,
-        "next"
-      );
-      ButtonList.push(day);
-    }
-    return ButtonList;
+  function buildData(
+    id,
+    name,
+    isToday,
+    hasEvent,
+    isSelected,
+    isInThisMonth,
+  ) {
+    const day = {
+      id: id,
+      name,
+      isToday,
+      hasEvent,
+      isSelected,
+      isInThisMonth,
+    };
+    return day;
   }
+
+  const ButtonList = [];
+
+  for (
+    let i = 1, j = daysInLastMonth - startDayOfMonth + 2;
+    i < startDayOfMonth;
+    i++, j++
+  ) {
+    const day = buildData(`${i}` + nanoid(), j, false, false, false, "last");
+    ButtonList.push(day);
+  }
+
+  let isTd,
+    isSl = false;
+  for (let i = 0; i < daysInMonth; i++) {
+    const eventI = currentUser
+      ? eventsInThisMonth.filter(
+          (event) => Number(event.date.slice(8, 10)) === i + 1
+        )
+      : [];
+
+    if (i + 1 === Number(today) && isThisMonth) {
+      isTd = true;
+    }
+    if (i + 1 === Number(gDay.slice(8, 10))) {
+      isSl = true;
+    }
+    const day = buildData(
+      String(i + startDayOfMonth) + nanoid(),
+      i + 1,
+      isTd,
+      eventI.length > 0,
+      isSl,
+      "this"
+    );
+    isTd = isSl = false;
+    ButtonList.push(day);
+  }
+  let numOfBtn;
+  ButtonList.length >= 35 ? numOfBtn=42 : numOfBtn=35
+
+
+  for (let i = ButtonList.length, j = 1; i < numOfBtn; i++, j++) {
+    const day = buildData(String(i) + nanoid(), j, false, false, false, "next");
+    ButtonList.push(day);
+  }
+  return ButtonList;
+}
